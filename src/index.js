2d5d8d260ba54cb53bd537fe54073475db970f84
jatin more <jatinmore077@gmail.com>
2024-01-29 19:00:59 +0530

    intial commit

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { makeServer } from './server'
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// Call make Server
+makeServer();
 root.render(
   <React.StrictMode>
     <App />
